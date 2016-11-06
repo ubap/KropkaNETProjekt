04ddd88a9a6986a3e7f9426d13434ec4b79c2fc2
Maniek <mmmanio93@gmail.com>
2016-11-06 17:20:34 +0100

    Initial commit

---
+++ Quizwanie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Quizwanie.MvcApplication" Language="C#" %>
---
+++ Quizwanie/Models/Question.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quizwanie.Models
+{
+    public class Question
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string WrongAnswer1 { get; set; }
+        public string WrongAnswer2 { get; set; }
+        public string WrongAnswer3 { get; set; }
+    }
+
+    public class QuestionDBContext: DbContext {
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
